19489f9ce9ee9de63d56784ab0b2e39b8b5a3351
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-12-31 14:12:55 +0500

    some work is done

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import FlashcardList from './FlashcardList';
 
 export default function App() {
-  return <div></div>;
+  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  // console.log(flashcards);
+  return (
+    <div>
+      <FlashcardList flashcards={flashcards} />
+    </div>
+  );
 }
 
 const SAMPLE_FLASHCARDS = [
   {
     id: 1,
-    question: 'what is 2 + 2',
+    question: 'what is 1 + 3',
     answer: '4',
     options: ['2', '3', '4', '6'],
   },
   {
     id: 2,
-    question: 'what is 2 + 2',
-    answer: '4',
-    options: ['2', '3', '4', '6'],
+    question: 'question 2',
+    answer: 'answer 2',
+    options: ['answer 2', 'answer 3', 'answer 4', 'answer 6'],
   },
 ];
+console.log(SAMPLE_FLASHCARDS);
