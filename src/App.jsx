8c1c21b97dbe6a2c2f431096794e718478769d65
ali9ahmad <temur.khan.dispatch@gmail.com>
2023-12-31 13:17:41 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function App() {
+  return <div></div>;
+}
+
+const SAMPLE_FLASHCARDS = [
+  {
+    id: 1,
+    question: 'what is 2 + 2',
+    answer: '4',
+    options: ['2', '3', '4', '6'],
+  },
+  {
+    id: 2,
+    question: 'what is 2 + 2',
+    answer: '4',
+    options: ['2', '3', '4', '6'],
+  },
+];
